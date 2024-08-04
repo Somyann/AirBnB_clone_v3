44019941ce9b6902942ad39f4342524e0fdddd30
Somyann <somiaprecious91@gmail.com>
2024-08-04 17:06:13 +0100

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-""" Module containing views """
-from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify
 from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
-@app_views.route('/api/v1/stats', methods=["GET"],  strict_slashes=False)
+
+@app.route('/api/v1/stats', methods=['GET'])
 def get_stats():
-    """ returns a JSON """
-    data = {"status": "OK"}
-    
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
-
-@app_views.route('/api/v1/stats', methods=["GET"], strict_slashes=False)
-def stats():
-    """ retrieves the number of each objects by type """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+    """
+    Endpoint that retrieves the number of each object by type.
+    """
+    stats = {
+        'Amenity': storage.count('Amenity'),
+        'City': storage.count('City'),
+        'Place': storage.count('Place'),
+        'Review': storage.count('Review'),
+        'State': storage.count('State'),
+        'User': storage.count('User')
     }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
     return jsonify(stats)
+
 if __name__ == '__main__':
     app.run(debug=True)
